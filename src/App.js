2f8c51c59beb64b0ff1405730469d5b76c125f06
Guilherme Santos <guiferrera@brq.com>
2021-09-21 20:57:51 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import img01 from "./assets/images/01.jpg";
+import img02 from "./assets/images/03.jpg";
+import sharedIcon from "./assets/images/icon-share.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="cols-left">
+        <img src={img01} alt="" />
+      </div>
+      <div className="cols-right">
+        <div className="title">
+          <h1>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium
+          </h1>
+        </div>
+        <div className="text">
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+          </p>
+        </div>
+        <div className="shared">
+          <div className="cols-left">
+            <img src={img02} alt="" />
+            <div>
+              <b>Guilherme Santos</b>
+              <small>21 Jun 1995</small>
+            </div>
+          </div>
+          <div className="cols-right">
+            <img src={sharedIcon} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
